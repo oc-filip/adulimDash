ce253026ed5b291b120df39dafc86ac6c8aca6c2
oc-filip <132068123+oc-filip@users.noreply.github.com>
2023-11-20 10:18:38 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ module.exports = (phase, { defaultConfig }) => {
     },
     env: {
         //NEXTAUTH_URL :"https://dash.star.co.il",
-        API_URL: "https://dash.star.co.il/api",
+        API_URL: "https://adulim-dash.vercel.app/api",
         MONGO_URI: "mongodb+srv://adulim:uMsoU1KgvAz9QwYf@cluster0.568vwt7.mongodb.net/AdulimDB?retryWrites=true&w=majority",
         JWT_SECRET: "ou5389nvx@'1=!lsdgkwrg",
         WC_URL: "https://adulim.co.il",
