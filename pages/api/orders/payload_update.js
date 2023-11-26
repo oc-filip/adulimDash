7c5c342b22b512625df9d2708f1d14b5afb1cf41
oc-filip <132068123+oc-filip@users.noreply.github.com>
2023-11-26 23:38:36 +0100

    Create payload_update.js

---
+++ pages/api/orders/payload_update.js
@@ -0,0 +1,29 @@
+import dbConnect from '../../../utils/dbConnect';
+import Order from '../../../models/order';
+
+dbConnect();
+
+export default async (req, res) => {
+    const { method } = req;
+
+    switch (method) {
+            case 'POST':
+            try {
+
+               const id = req.body.id;
+
+
+                const updatedOrder= await Order.findOneAndUpdate({id:id}, req.body);
+                
+                res.status(200).json({ success: true, data: updatedOrder , msg: "Order Updated"});
+               
+
+            } catch (error) {
+                res.status(400).json({ success: false , msg: error});
+            }
+            break;
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+}
