700e8f54a928bdf5ab5d532d077129c856d0193b
oc-filip <132068123+oc-filip@users.noreply.github.com>
2023-11-26 23:05:42 +0100

    Update person.js

--- models/person.js
+++ models/person.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
-
 const PersonSchema = new mongoose.Schema({
-    id: { type: Number , unique: true },
-},{ collection: 'new_persons' ,strict: false});
+    id: { type: Number },
+    first_name: { type: String  },
+    last_name: { type: String  },
+    b38f26908a473cb2993a861d9590b687a9a0ca15: { type: Number  },
+
+},{ collection: 'Persons' ,strict: false});
+
 
 
 module.exports = mongoose.models.Person || mongoose.model("Person", PersonSchema);
