f2d73ad0272879d1d7328ee27b21e229bd3cb89d
oc-filip <132068123+oc-filip@users.noreply.github.com>
2023-11-26 01:25:53 +0100

    Update person.js

--- models/person.js
+++ models/person.js
@@ -2,13 +2,7 @@ const mongoose = require('mongoose');
 
 const PersonSchema = new mongoose.Schema({
     id: { type: Number , unique: true },
-    first_name: { type: String },
-    last_name: { type: String },
-    email: { type: Array },
-    name: { type: String },
-    phone: { type: Array }
-    
-},{ collection: 'new_persons' });
+},{ collection: 'new_persons' ,strict: false});
 
 
 
