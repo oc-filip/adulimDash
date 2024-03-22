92cd145cdfeef0f9e91b7ee7624242e368819aef
oc-filip <132068123+oc-filip@users.noreply.github.com>
2024-03-22 21:30:58 +0100

    Update get_persons.js

--- pages/api/crons/get_persons.js
+++ pages/api/crons/get_persons.js
@@ -7,16 +7,16 @@ dbConnect();
 export default async (req, res) => {
 
     try {
-
+/*
     const p = await fetch(`https://api.pipedrive.com/v1/persons?api_token=5bafa4354cf7e47f6dbfcd414d36877fbe1fa68a&start=0&limit=500`);
     const persons = await p.json();
 
     
     const data = await Persons.create(persons);
+*/
+    console.log('persons cron run')
 
-    console.log('persons', data)
-
-    res.status(201).json({ success: true, status:"saved", data: data })
+    res.status(201).json({ success: true, status:"saved",  })
 
 
 
